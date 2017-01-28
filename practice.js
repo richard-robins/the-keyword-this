b8fe4fefdf06333b3b8ff2280e85c9a3f956fb41
Richard <richard.robins@gmail.com>
2017-01-28 01:31:44 -0700

    everything but the last one works

--- practice.js
+++ practice.js
@@ -2,19 +2,21 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
-
+//The purpose of the the "this keyword is to allow reference back to an object by using context"
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
+//implicit, explicit, new, default
 
   // 3) What is the difference between call and apply?
 
       //Answer
+//call uses parameters that are explicitly given and apply uses an array
 
   // 4) What does .bind do?
 
       //Answer
-
+      //it creates a link betweeen an object and parameter
 
 //Next Problem
 
@@ -24,8 +26,18 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+    var user =  {
+      username: "John",
+      email: "John@email.com",
+      getUsername: function(){
+        return this.username;
+      }
+
+
+    }
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
+user.getUsername();
 
 
 //Next Problem
@@ -34,6 +46,13 @@
 // Write the function definitions which will make the following function invocations function properly.
 
   //Function Invocations Here
+function Car(make, model, year){
+  this.move = 0;
+  this.moveCar = function(){
+    return this.move = this.move + 10;
+  }
+}
+
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -69,7 +88,8 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.bind(myUser);
+userName(); //Fix this
 
 //Above you're given an object, and  a function. What will the getUsername function return?
 //Note(no tests)
